bdb3e6d8a15b055e812e5c162dc5bce7d7cc30cb
TamzidHossen <tamzidhossen366@gmail.com>
2024-09-07 19:31:59 +0600

    SoundMate Project

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend:  {
+            colors: {
+                "logo" : {
+                    700: '#0e7490',
+                    800: '#155e75',
+                }
+            }
+        }
+    }
+}
